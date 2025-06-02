bf40f621178be269938c33e3c42960f559e525b9
Chirag Shetty <business.chiragbp@gmail.com>
2025-06-02 12:47:50 +0530

    Update teslaData.ts

--- src/utils/data/teslaData.ts
+++ src/utils/data/teslaData.ts
@@ -2,5 +2,109 @@
 import { Product } from '../productData';
 
 export const teslaProducts: Product[] = [
-  // You can add Tesla products here in the future
+  {
+    "id":1,
+    "name":"1\/18 Tesla Diecast - Cyber truck in Striking Silver Finish Model Car",
+    "image":"https:\/\/cdn.shopify.com\/s\/files\/1\/1786\/0103\/files\/photo_5836921587205521409_y.jpg?v=1742563846",
+    "price":0,
+    "category":"tesla"
+  },
+  {
+    "id":2,
+    "name":"Tesla Model Y [ 1\/18 White, Diecast ]",
+    "image":"https:\/\/cdn.shopify.com\/s\/files\/1\/1786\/0103\/files\/photo_6_2025-01-15_15-57-44.jpg?v=1742563479",
+    "price":0,
+    "category":"tesla"
+  },
+  {
+    "id":3,
+    "name":"Tesla Model 3",
+    "image":"https:\/\/cdn.shopify.com\/s\/files\/1\/1786\/0103\/products\/tesla3black1.jpg?v=1742565991",
+    "price":0,
+    "category":"tesla"
+  },
+  {
+    "id":4,
+    "name":"1\/18 Diecast Tesla Roadster in Red",
+    "image":"https:\/\/cdn.shopify.com\/s\/files\/1\/1786\/0103\/products\/teslaroadster1.jpg?v=1747135457",
+    "price":0,
+    "category":"tesla"
+  },
+  {
+    "id":5,
+    "name":"Tesla Model S",
+    "image":"https:\/\/cdn.shopify.com\/s\/files\/1\/1786\/0103\/products\/photo1673453074-6.jpg?v=1742565936",
+    "price":0,
+    "category":"tesla"
+  },
+  {
+    "id":6,
+    "name":"1\/18 Tesla Model 3 Grey Model car",
+    "image":"https:\/\/cdn.shopify.com\/s\/files\/1\/1786\/0103\/products\/photo1673453061-9.jpg?v=1742565938",
+    "price":0,
+    "category":"tesla"
+  },
+  {
+    "id":7,
+    "name":"1\/18 Tesla Model 3 [ Red, Diecast ]",
+    "image":"https:\/\/cdn.shopify.com\/s\/files\/1\/1786\/0103\/products\/photo1673447226-2.jpg?v=1742565968",
+    "price":0,
+    "category":"tesla"
+  },
+  {
+    "id":8,
+    "name":"Tesla Model S White",
+    "image":"https:\/\/cdn.shopify.com\/s\/files\/1\/1786\/0103\/products\/teslamodelswhite1.jpg?v=1742566160",
+    "price":0,
+    "category":"tesla"
+  },
+  {
+    "id":9,
+    "name":"Tesla Model 3",
+    "image":"https:\/\/cdn.shopify.com\/s\/files\/1\/1786\/0103\/products\/teslamodel3white1.jpg?v=1742566164",
+    "price":0,
+    "category":"tesla"
+  },
+  {
+    "id":10,
+    "name":"Tesla Model 3",
+    "image":"https:\/\/cdn.shopify.com\/s\/files\/1\/1786\/0103\/products\/teslamodel3blue1.jpg?v=1742566308",
+    "price":0,
+    "category":"tesla"
+  },
+  {
+    "id":11,
+    "name":"1\/18 Diecast Tesla Semi Truck Silver (opening doors)",
+    "image":"https:\/\/cdn.shopify.com\/s\/files\/1\/1786\/0103\/products\/teslasemi1.jpg?v=1742565989",
+    "price":0,
+    "category":"tesla"
+  },
+  {
+    "id":12,
+    "name":"Tesla Model S Grey",
+    "image":"https:\/\/cdn.shopify.com\/s\/files\/1\/1786\/0103\/products\/teslamodelsgrey1.jpg?v=1742566161",
+    "price":0,
+    "category":"tesla"
+  },
+  {
+    "id":13,
+    "name":"Tesla Model S",
+    "image":"https:\/\/cdn.shopify.com\/s\/files\/1\/1786\/0103\/products\/teslamodelsblack1.jpg?v=1742566163",
+    "price":0,
+    "category":"tesla"
+  },
+  {
+    "id":14,
+    "name":"Tesla Model S Red",
+    "image":"https:\/\/cdn.shopify.com\/s\/files\/1\/1786\/0103\/products\/teslamodelsred1.jpg?v=1742566305",
+    "price":0,
+    "category":"tesla"
+  },
+  {
+    "id":15,
+    "name":"Tesla Model S Blue",
+    "image":"https:\/\/cdn.shopify.com\/s\/files\/1\/1786\/0103\/products\/teslamodelsblue1.jpg?v=1742566306",
+    "price":0,
+    "category":"tesla"
+  }
 ];
